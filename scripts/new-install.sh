5ff2e326455f4a4423791940aa50d05a366cdc9d
asongent <59109755+asongent@users.noreply.github.com>
2023-07-10 11:42:36 -0400

    Add files via upload
    bug fixed

---
+++ scripts/new-install.sh
@@ -0,0 +1,79 @@
+#!bin/bash
+
+## Update packages and disbale swap
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https curl
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+## Set Up echo Add  iptables
+sudo cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward = 1
+EOF
+sudo sysctl --system
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
+modprobe br_netfilter
+sysctl -p /etc/sysctl.conf
+
+### Download Containerd Binaries 
+sudo wget https://github.com/containerd/containerd/releases/download/v1.7.2/containerd-1.7.2-linux-amd64.tar.gz
+sudo tar Cxzvf /usr/local containerd-1.7.2-linux-amd64.tar.gz
+sudo rm -rf containerd-1.7.2-linux-amd64.tar.gz
+
+## Setup container and restart Containerd
+wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+sudo mkdir -p /usr/local/lib/systemd/system
+sudo mv containerd.service /usr/local/lib/systemd/system/containerd.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now containerd
+
+## Install Runtime Class
+wget https://github.com/opencontainers/runc/releases/download/v1.1.7/runc.amd64
+sudo install -m 755 runc.amd64 /usr/local/sbin/runc
+
+##Insttall CNI plugins
+sudo mkdir -p /opt/cni/bin
+wget https://github.com/containernetworking/plugins/releases/download/v1.3.0/cni-plugins-linux-amd64-v1.3.0.tgz
+sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.3.0.tgz
+sudo rm -rf cni-plugins-linux-amd64-v1.3.0.tgz
+
+
+
+##Install CRITCL
+VERSION="v1.26.0"
+wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
+sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
+rm -f crictl-$VERSION-linux-amd64.tar.gz
+
+sudo touch /etc/crictl.yaml
+sudo cat <<EOF | sudo tee /etc/crictl.yaml
+runtime-endpoint: unix:///run/containerd/containerd.sock
+image-endpoint: unix:///run/containerd/containerd.sock
+timeout: 2
+debug: false
+pull-image-on-create: false
+EOF
+
+##Install kubectl, kubelet and kubeadm
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+
+###Install k8s binaeries
+sudo apt-get update -y
+sudo apt -y install kubelet=1.25.3-00 kubeadm=1.25.3-00 kubectl=1.25.3-00
+sudo apt-mark hold kubelet kubeadm kubectl 
+
+
